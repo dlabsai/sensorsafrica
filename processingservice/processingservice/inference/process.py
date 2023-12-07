484f74341089bc1da8e4bf84b5241731fd9d6619
mkarpicz <maciej.karpicz@gmail.com>
2023-12-07 19:54:12 +0100

    add more logging

--- processingservice/processingservice/inference/process.py
+++ processingservice/processingservice/inference/process.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import logging
 
 from typing import Any, Dict
 from ..awsadapters.db import update_dynamodb_item
@@ -7,29 +8,53 @@ from ..data.transforms.dataset import build_input_records, get_datasets
 from .enums import InferenceStatus
 from .loaders import load_user_uploaded_data_from_storage, load_predictors
 
+logger = logging.getLogger(__name__)
+
 
 def process(payload: Dict[str, Any]) -> bool:
+    request_id = payload["RequestID"]
     # Load file from S3
-    frame_with_data = load_user_uploaded_data_from_storage(payload["S3location"])
+    try:
+        frame_with_data = load_user_uploaded_data_from_storage(payload["S3location"])
+    except Exception as e:
+        logger.error(f"Failed to load file from S3 for request ID {request_id} : {str(e)}")
+        update_record_status(request_id, InferenceStatus.FAILED)
+        return True
 
-    input_records = build_input_records(csv_as_df=frame_with_data)
+    # Build input records
+    try:
+        input_records = build_input_records(csv_as_df=frame_with_data)
+    except Exception as e:
+        logger.error(f"Failed to build input records for request ID {request_id} : {str(e)}")
+        update_record_status(request_id, InferenceStatus.FAILED)
+        return True
     dataset_for_inference = get_datasets(input_records)
 
     # Make inference
-    result = run_inference(*dataset_for_inference)
+    try:
+        result = run_inference(*dataset_for_inference)
+    except Exception as e:
+        logger.error(f"Failed to run inference for request ID {request_id} : {str(e)}")
+        update_record_status(request_id, InferenceStatus.FAILED)
+        return True
 
     # Save inference to S3
     print(result)
 
-    # Update record in DynamoDB
-    update_dynamodb_item(table_name="sensorsafrica-requests",
-                         key={"RequestID": payload["RequestID"]},
-                         update_expression="SET #status = :status",
-                         expression_attribute_values={":status": InferenceStatus.SUCCESS.value},
-                         expression_attribute_names={"#status": "Status"}
-                         )
+    # Update DynamoDB record
+    update_record_status(payload["RequestID"], InferenceStatus.SUCCESS)
 
     return False  # TODO: change to True after testing
+
+
+def update_record_status(request_id: str, status: InferenceStatus) -> None:
+    # Update record in DynamoDB
+    update_dynamodb_item(table_name="sensorsafrica-requests",
+                         key={"RequestID": request_id},
+                         update_expression="SET #status = :status",
+                         expression_attribute_values={":status": status.value},
+                         expression_attribute_names={"#status": "Status"}
+                         )
 
 
 def run_inference(df_pm_1: pd.DataFrame, df_pm_2_5: pd.DataFrame, df_pm_10: pd.DataFrame) -> pd.DataFrame:
